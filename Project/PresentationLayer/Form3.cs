f5eb4694c37debf502a3baea0c91be900a0daae6
LuanBosch <91474771+LuanBosch@users.noreply.github.com>
2021-10-13 11:45:55 +0200

    Add files via upload

---
+++ Project/PresentationLayer/Form3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form3 : Form
+    {
+        List<Module> s = new List<Module>();
+        DataHandler dh = new DataHandler();
+        public static string code;
+        public static bool InsertOrUpdate;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 nf = new Form2();
+            nf.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InsertOrUpdate = true;           
+            Form5 nf = new Form5();
+            nf.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InsertOrUpdate = false;
+            Form5 nf = new Form5();
+            nf.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dh.DeleteModule(code);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                DataGridViewRow r = this.dataGridView1.Rows[e.RowIndex];
+                code = r.Cells["ModuleCode"].Value.ToString();
+            }
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dh.GetModule();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dh.SearchModule(code);
+        }
+    }
+}
---
+++ Project/PresentationLayer/Form5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form5 : Form
+    {
+        List<string> modules = new List<string>();
+        BindingSource bs = new BindingSource();
+        DataHandler dh = new DataHandler();
+        public static string code, name, links, descriptions;
+        public Form5()
+        {
+            InitializeComponent();
+            bs.DataSource = modules;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Module md = new Module((textBox1.Text), (textBox2.Text), (textBox8.Text),(richTextBox1.Text));  
+            if (Form3.InsertOrUpdate == true)
+            {
+                dh.AddModule(Form5.code, Form5.name, Form5.links, Form5.descriptions);
+            }
+            else
+            {
+                dh.UpdateModule(Form5.code, Form5.name, Form5.links, Form5.descriptions);
+            }
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form5_Shown(object sender, EventArgs e)
+        {
+            code = textBox1.Text;
+            name = textBox2.Text;
+            links = textBox8.Text;
+            descriptions = richTextBox1.Text;
+        }
+    }
+}
